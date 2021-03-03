a20d840fe7932ba14d2b9fa0590d75074e564c2c
zsh2401 <zsh2401@163.com>
2021-03-03 15:41:23 +0800

    introduced more dev tools

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
+/**
+ * Copyright (c) 2021 Seymour Zhang and other contributors.
+ * tdscore is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *       http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ * Mulan Permissive Software License，Version 2
+ */
 export { default as DSArray, from } from "./DSArray"
 export { default as DSObject } from "./DSObject"
 export { default as DSEvent } from "./DSEvent"

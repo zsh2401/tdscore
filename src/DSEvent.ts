88699b41dfa631c98f972a6faf08b1d03b1e4b33
zsh2401 <zsh2401@163.com>
2021-03-04 22:30:47 +0800

    New testing feature of DSEvent: password

--- src/DSEvent.ts
+++ src/DSEvent.ts
@@ -3,7 +3,7 @@ import HashSet from "./data-structure/HashSet";
 import ICollection from "./data-structure/ICollection";
 import ISet from "./data-structure/ISet";
 import DSObject from "./DSObject";
-
+import sha256 from "./util/sha256"
 export interface Handler<TArgs, TReturn> {
     (sender: any, args: TArgs): TReturn;
 }
@@ -12,10 +12,6 @@ export class EventArgs extends DSObject {
 
 }
 
-const DEFAULT_PASSWORD = "";
-function sha256(str: string): string {
-
-}
 export default class DSEvent<A = EventArgs, R = void> extends DSObject {
 
     private readonly listeners: ICollection<Handler<A, R>>;
@@ -24,9 +20,13 @@ export default class DSEvent<A = EventArgs, R = void> extends DSObject {
     constructor(password?: string) {
         super();
         this.listeners = new LinkedList();
-        this.passwordSha256 = password === undefined ? null : sha256(password);
+        this.passwordSha256 = password === undefined ? null : (sha256(password) as string);
     }
 
+    get isProtectedByPassword() {
+        return this.passwordSha256 !== null;
+    }
+    
     private passwordIsCorrect(pwd?: string): boolean {
         if (this.passwordSha256) {
             if (pwd) {

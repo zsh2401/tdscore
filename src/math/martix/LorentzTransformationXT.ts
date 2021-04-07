6fca0bd6d6f31a193ab25177564089bfafb0ad8d
zsh2401 <zsh2401@163.com>
2021-04-07 22:27:22 +0800

    🦄refactor(math/martix): Changed the structure of martix module
    Martix related Object is based on RawMartix and algorithms now.

---
+++ src/math/martix/LorentzTransformationXT.ts
@@ -0,0 +1,14 @@
+import Vector2 from "./Vector2";
+import Transformation from "./Transformation";
+
+export default class LorzentzTransformationXT extends Transformation {
+    constructor(x: number, t: number, c: number = 1) {
+        super(...LorzentzTransformationXT.getVector(x, t, c));
+    }
+    private static getVector(x: number, t: number, c: number): [Vector2, Vector2] {
+        const v = (x / t)
+        const r = 1 / ((1 - (v ** 2 / c ** 2)) ** 0.5);
+        const u = v / c;
+        return [new Vector2(r, -r * u), new Vector2(-r * u, r)]
+    }
+}
